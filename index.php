61633cd75a7fd0a4145159a4bb0383583b425571
acano <acano@csumb.edu>
2017-08-25 20:21:38 +0000

    adding index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+        <h1>Welcome to Andrea's CST336 Webpage</h1>
+    </body>
+</html>
