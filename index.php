6e6fbdefc75e484c8ceddf53e09cfa7e0f924616
acano <acano@csumb.edu>
2017-08-31 00:31:48 +0000

    Adding details such as rainbows and too much movement.

--- index.php
+++ index.php
@@ -1,77 +1,17 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Andrea Cano's Portfolio</title>
+        <title> Andrea Cano's Portfolio </title>
+        <img src="https://media1.giphy.com/media/nOAje1WfEtLmU/200_s.gif">
         <meta charset="utf-8"/>
     </head>
-    <body style = "background-color:#99D5CF">
-        <header>
-            <style>
-                h1{
-                    text-align:center;
-                }
-            </style>
-            <strong><h1>Andrea Cano</h1></strong>
-            
-        </header>
-    <div>
+    <body>
+        <head>
+            <h1>Andrea Cano</h1>
+        </head>
         <nav>
-           <style>
-           ul {
-               text-align:center;
-               list-style-type: none;
-               margin: 0;
-               padding: 0;
-               overflow: hidden;
-           }
-           li {
-                display:inline-block;
-            }
-            li a {
-                display: block;
-                color: green;
-                padding: 14px 16px;
-                text-decoration: none;
-            }
-            li a:hover {
-                background-color: #111;
-            }
-           </style>
-            <ul>
-            <li><a href = "index.html">Home</a></li>
-            <li><a href = "about.html">About</a></li>
-            <li><a href = "contact.html">Contact</a></li>
-            </ul>
+            
         </nav>
-    </div>
-    
-    <div>
-        <section>
-            <img src="img/Myface.png" alt="Profile Picture">
-        </section>
-        <aside>
-           <p>
-               Hi, Welcome to my portfolio
-               <br>
-               I haven't had many achievements 
-               <br>
-               BUT
-               <br>
-               I am working on it!
-           </p>
-        </aside>
-     </div>
-    
-    <style>
-                footer{
-                    text-align:center;
-                }
-            </style>
-        <footer>
-            <hr>
-                CST336. 2017&copy; Cano<br />
-                <strong>Disclaimer:</strong> The information on this webpage is ficticious.<br />
-                It is used for academic purposes.
-        </footer>
+        
     </body>
 </html>
